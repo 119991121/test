f700d6cab7b08e5f16b4cfc4fd2fd7d8879c6a41
ryougi-misaki <https://github.com/ryougi-misakki/longdashuaib.git>
2020-10-15 14:16:43 +0800

    优化版本

--- 龙伟/js/2.js
+++ 龙伟/js/2.js
@@ -1,13 +1,17 @@
 // JavaScript
-function changeColor2(x) {
-    x.style.background = '#ec971f';
-}
-function changeColorBack2(x) {
-    x.style.background = '#f0ad4e';
-}
-function changeColor1(x) {//光标移动高亮事件（实现）
-    x.style.background = '#3071a9';
-}
-function changeColorBack1(x) {
-    x.style.background = '#428bca';
-}
+var btn = document.getElementsById('button1');
+function run(){ 
+  var time = 60;
+  timer = setInterval(
+    function(){ 
+      btn.disabled = true;
+      btn.innerText = time+"秒后重新发送";
+      time--;
+      if(time ==0){ 
+      btn.innerText='重新发送验证码';
+      btn.disabled = false;
+      clearInterval(timer);
+      } 
+    }
+	,6000) 
+} 
